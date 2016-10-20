18a35f5c86050b7f8920676969e34d96ac584b21
xiedairong <xiedrsz@sinosoft.com.cn>
2016-10-20 17:10:50 +0800

    xvue插件

--- requirejs/myplugin/xvue/xvue.js
+++ requirejs/myplugin/xvue/xvue.js
@@ -16,8 +16,8 @@ define(['text'], function (text) {
 	function getDeps() {
 		var res = scriptStr.match(Deps_reg),
 			temp,
-			Deps_name_list = '(Vue',
-			Deps_path_list = "['Vue'";
+			Deps_name_list = '(Vue, ',
+			Deps_path_list = "['Vue', ";
 
 		// import
 		if (!!res) {
@@ -29,17 +29,14 @@ define(['text'], function (text) {
 				Deps_path_list += "'xvue!" + temp[1].replace(/\'|\"/g, '') + ".xvue', ";
 				Deps_name_list += temp[0] + ", ";
 			});
-			// 添加到依赖列表
-			Deps_path_list = Deps_path_list.slice(0, -2) + ']';
-			Deps_name_list = Deps_name_list.slice(0, -2) + ')';
-		} else {
-			Deps_path_list += ']';
-			Deps_name_list += ')';
 		}
+		// 添加到依赖列表
+		Deps_path_list = Deps_path_list.slice(0, -2) + ']';
+		Deps_name_list = Deps_name_list.slice(0, -2) + ')';
 
 		// export
 		scriptStr = scriptStr.replace(export_reg, 'return Vue.extend({\r\ntemplate: "' + tempStr + '",');
-		
+
 		scriptStr = "define(" + Deps_path_list + ", function " + Deps_name_list + " {'use strict';\r" + scriptStr + ");\r\n});";
 	}
 
@@ -56,6 +53,7 @@ define(['text'], function (text) {
 		req(['text!' + name], function (value) {
 			divide(value);
 			getDeps();
+			//			console.log(scriptStr);
 			onload.fromText(scriptStr);
 		});
 	};

29d9d0a24afda4b21a2faa132c4b6495b1cb4fb3
xiedairong <xiedrsz@sinosoft.com.cn>
2016-11-11 15:16:44 +0800

    fam libs API
    fam 工具新增module功能（目前只针对vue）;
    收藏  weinre 远程调试的脚本；
    收藏 API 调用记录。

--- fam/index.js
+++ fam/index.js
@@ -1,36 +1,42 @@
 #!/usr/bin/env node
 
-const util = require('./common/util'),
-	version = require('./info/version'),
-	help = require('./info/help'),
-	init = require('./tool/init'),
-	build = require('./tool/build'),
-	table = require('./tool/table');
+var
+    util = require('./common/util'),
+    version = require('./info/version'),
+    help = require('./info/help'),
+    init = require('./tool/init'),
+    build = require('./tool/build'),
+    table = require('./tool/table'),
+    module = require('./tool/module');
 
 var agrv = util.argv(),
-	func = agrv.func;
+    func = agrv.func;
 
 switch (func) {
-	// 帮助说明
-	case undefined:
-	case "-help":
-		help.show();
-		break;
-	// 版本
-	case "-v":
-	case "-version":
-		version.show();
-		break;
-	// 初始化工程
-	case "init":
-		init.init();
-		break;
-	// 构建工程
-	case "build":
-		build.build();
-		break;
+    // 帮助说明
+case undefined:
+case "-help":
+    help.show();
+    break;
+    // 版本
+case "-v":
+case "-version":
+    version.show();
+    break;
+    // 初始化工程
+case "init":
+    init.init();
+    break;
+    // 构建工程
+case "build":
+    build.build();
+    break;
     // 新建表
-    case "table":
-        table.add(agrv.args);
-        break;
+case "table":
+    table.add(agrv.args);
+    break;
+    // 新建模块
+case "module":
+    module.distribute(agrv.args);
+    break;
 }
